428b5605d778e9974266f5aa85aeaca603a0e9df
Piyush1817 <lakwanisuresh086@gmail.com>
2026-01-04 17:09:09 +0530

    Pattern4 completed

--- Loops/Pattern3.java
+++ Loops/Pattern3.java
@@ -1,3 +1,9 @@
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+
 import java.util.Scanner;   
 public class Pattern3 {
     public static void main(String[] args) {
---
+++ Loops/Pattern4.java
@@ -0,0 +1,16 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n = 5;
+        Pattern(n);
+    }
+   static void Pattern(int n) {
+        for (int row = 1; row <= n; row++) {
+            for(int col=1;col<=row;col++){
+                System.out.print(col);
+
+            }
+            System.out.println();
+        }
+    }
+            
+}
