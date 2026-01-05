307b3a49fb1e2d4d2c80d2440656ceb2a3df1535
Piyush1817 <lakwanisuresh086@gmail.com>
2026-01-05 21:17:23 +0530

    added 6th pattern

---
+++ Loops/Pattern6.java
@@ -0,0 +1,27 @@
+ import java.util.Scanner;
+ public class Pattern6 {
+     @SuppressWarnings("ConvertToTryWithResources")
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the number of rows: ");
+         int n = sc.nextInt();
+         Pattern(n);
+         sc.close();
+     }
+
+     static void Pattern(int n) {
+         for (int row = 1; row <=n; row++) {
+             for (int space = 1; space <= n - row; space++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int star = 1; star <= row; star++) {
+                System.out.print("*");
+            }
+             System.out.println();
+
+             
+     }
+ }
+ }
