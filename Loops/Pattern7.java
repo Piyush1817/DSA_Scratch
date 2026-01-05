f53227317d22045f503b01c4a4d4a7e984eae274
Piyush1817 <lakwanisuresh086@gmail.com>
2026-01-06 00:18:12 +0530

    added pattern8

--- Loops/Pattern7.java
+++ Loops/Pattern7.java
@@ -1,13 +1,20 @@
 
+
+// ******
+//  *****
+//   ****
+//    ***
+//     **
+//      *
 import java.util.Scanner;
 
 public class Pattern7 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc7 = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
-        int n = sc.nextInt();   
+        int n = sc7.nextInt();   
         Pattern(n);
-        sc.close(); 
+        sc7.close(); 
     }
     static void Pattern(int n ){
         for(int row=1;row<=n;row++){
---
+++ Loops/Pattern8.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        sc.close();
+        Pattern(n);
+    }
+    static void Pattern(int n){
+        for(int row=1;row<=n;row++){
+            for (int space=1;space<n-row+1;space++){
+                System.out.print(" ");
+            }
+            for (int star=1;star<=2*row-1;star++){
+                System.out.print("*");
+            }
+        System.out.println();
+     
+    } 
+} 
+}
+
