0f01fab3de1796c809507088f5139713ab9b8d48
Piyush1817 <lakwanisuresh086@gmail.com>
2026-01-08 23:39:27 +0530

    added return programs

---
+++ functions/Return.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Return {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = product(sc);
+        sc.close();
+    
+        System.out.println("The product is: " + result);
+        System.out.println(getGreeting());
+    }
+
+     static int product(Scanner sc   ){
+        System.out.println("Enter two numbers for multiplication:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        
+        return a*b;
+    }
+    static String getGreeting(){
+        return "Hello, welcome to the multiplication program!";
+    }   
+
+}
