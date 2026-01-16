fd7e5ddccbe91fc9df96ee5db81158bf21f49838
Piyush1817 <lakwanisuresh086@gmail.com>
2026-01-16 17:30:15 +0530

    done with method overloading

---
+++ functions/Overloading.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Overloading {
+    public static void main(String[] args) {
+        idk(67);
+       idk("Piyush");
+       idk(5,876543);
+    }
+    static void idk(int a){
+        System.out.println("This is integer method "+a);
+    }
+    static void idk(String a){
+        System.out.println("This is string method "+a);
+    }
+    static int idk(int a,int b){
+        return a+b;
+    }
+    static void idk(int...v){
+        System.out.println(Arrays.toString(v));
+    }
+
+ 
+}
