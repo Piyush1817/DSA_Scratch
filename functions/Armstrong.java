d3abbdbf0a2a3400535343aad852de22edfe89b9
Piyush1817 <lakwanisuresh086@gmail.com>
2026-01-16 17:52:48 +0530

    Armstrong number

---
+++ functions/Armstrong.java
@@ -0,0 +1,21 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        // int num = 153;
+        // System.out.println(isArmstrong(num));
+        for (int i = 100; i < 1000; i++) {
+            if (isArmstrong(i)) {
+                System.out.println(i + " is an Armstrong number.");
+            }
+        }
+    }
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+        return sum == original; 
+    }
+}
