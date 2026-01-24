f4cf82b7f933d585c79ecc271517a3b65084ec6a
Piyush1817 <lakwanisuresh086@gmail.com>
2026-01-24 13:39:39 +0530

    arrays are mutable but strings are not

---
+++ arrays/PassingValues.java
@@ -0,0 +1,11 @@
+public class PassingValues {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        System.out.println("Before modifying array: "+java.util.Arrays.toString(arr));
+        modifyArray(arr);
+        System.out.println("After modifying array: "+java.util.Arrays.toString(arr));
+    }    
+    static void modifyArray(int[] array){
+        array[0] = 99;
+    }
+}
