966b6488b53a9be007caa39f50dba8b898f703d0
Piyush1817 <lakwanisuresh086@gmail.com>
2026-02-02 07:27:09 +0530

    Starting with strings

---
+++ strings/Strings1.java
@@ -0,0 +1,10 @@
+package strings;
+           import java.util.*;
+           public class Strings1{
+            public static void main(String[] args ){
+                     Scanner sc = new Scanner(System.in );
+                     System.out.println("Enter your name :");
+                     String name = sc.nextLine();
+                     System.out.println("hello "+name+" welcome to this world");
+            }
+           }
