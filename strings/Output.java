eab5c894150f2e47a5154e31951bb43e92110727
Piyush1817 <lakwanisuresh086@gmail.com>
2026-02-03 20:42:31 +0530

    arrays.toString

---
+++ strings/Output.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class Output {
+    public static void main(String[] args ){
+        //for Arrays.toString() method 
+        String[] arr = {"Java", "Python", "JavaScript"};
+        System.out.println(java.util.Arrays.toString(arr)); // it will print the elements of the array in a readable format     
+        System.out.println(arr);//oin output it will print the address of the array or reference of the array
+
+    }
+    
+}
