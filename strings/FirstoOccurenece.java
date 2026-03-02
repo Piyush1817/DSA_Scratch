6fa12703a1084c8663dc2557fb8c639ee2d28639
Piyush1817 <lakwanisuresh086@gmail.com>
2026-03-02 13:25:17 +0530

    first occurence with manual method

--- strings/FirstoOccurenece.java
+++ strings/FirstoOccurenece.java
@@ -10,8 +10,29 @@ public class FirstoOccurenece {
         int index = firstOccurenceDirectly(mainString, subString);
         System.out.println("The first occurrence of \"" + subString + "\" in \"" + mainString + "\" is at index: " + index  );
         
+        int index2 = firstOccurenceManually(mainString, subString);
+        System.out.println("The first occurrence of \"" + subString + "\" in \"" + mainString + "\" using manual method is at index: " + index2  );
+        sc.close();
     }
     static int firstOccurenceDirectly(String mainString,String subString){    
             return    mainString.indexOf(subString);
+    }
+
+    static int firstOccurenceManually(String mainString,String subString){
+        int mainLength = mainString.length();
+        int subLength = subString.length();
+        
+        for(int i=0; i<=mainLength - subLength; i++){
+            int j;
+            for(j=0; j<subLength; j++){
+                if(mainString.charAt(i+j) != subString.charAt(j)){
+                    break;
+                }
+            }
+            if(j == subLength){
+                return i; // Found the substring at index i
+            }
+        }
+        return -1; // Substring not found
     }
 }
