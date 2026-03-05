27fb5c9b158645f570680c203ab1eb2deaf96b04
Piyush1817 <lakwanisuresh086@gmail.com>
2026-03-05 10:10:36 +0530

    other methods for plaindroime

--- strings/Palindrome.java
+++ strings/Palindrome.java
@@ -29,21 +29,21 @@ public class Palindrome {
         return true;
     }
 
-    // static boolean isPalindrome2(String str){
-    //     StringBuilder sb = new StringBuilder(str);
-    //     sb.reverse();
-    //     return sb.toString().equalsIgnoreCase(str);
-    // }
+    static boolean isPalindrome2(String str){
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        return sb.toString().equalsIgnoreCase(str);
+    }
 
-    // static boolean isPalindrome3(String str){
-    //   str.toLowerCase();
-    //   for(int i=0; i<str.length()/2; i++){
-    //   char start=str.charAt(0);
-    //     char end=str.charAt(str.length()-1-i);
-    //     if(start != end){
-    //         return false;
-    //     }
-    // }
-    // return true;
-    // }   
+    static boolean isPalindrome3(String str){
+      str.toLowerCase();
+      for(int i=0; i<str.length()/2; i++){
+      char start=str.charAt(0);
+        char end=str.charAt(str.length()-1-i);
+        if(start != end){
+            return false;
+        }
+    }
+    return true;
+    }   
 }
