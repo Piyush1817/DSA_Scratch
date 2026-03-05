95c7c463087a6abd3be9a97f72afb3f4f4787595
Piyush1817 <lakwanisuresh086@gmail.com>
2026-03-05 09:31:26 +0530

    last word count using pointer method

--- strings/CountLastWord.java
+++ strings/CountLastWord.java
@@ -3,9 +3,13 @@ package strings;
 public class CountLastWord {
     public static void main(String[] args) {
         String sentence = "Hello World";
+        String sentence2="My name is Piyush";
         int count = countLastWord(sentence);
         System.out.println("The number of characters in the last word is: " + count);
+         int count2=countLastWord2(sentence2);
+        System.out.println("The number of characters in the last word is: " + count2);
     }
+    
 
     static int countLastWord(String sentence) {
         String[] words = sentence.split(" ");
@@ -13,5 +17,16 @@ public class CountLastWord {
         return lastWord.length();
     }
 
-    
+    static int countLastWord2(String sentence){
+        int last = sentence.length()-1;
+        int count =0;
+        while (last>=0  && sentence.charAt(last)==' '){
+            last--;
+        }
+        while (last>=0  && sentence.charAt(last)!=' '){
+            count++;
+            last--;
+        }   
+        return count;
+    }
 }
