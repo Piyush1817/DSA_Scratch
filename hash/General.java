c1f47e520c501d64c87d0b9d8f5e8d3bfdd819a0
Piyush1817 <lakwanisuresh086@gmail.com>
2026-03-07 09:15:39 +0530

    tree map

--- hash/General.java
+++ hash/General.java
@@ -7,6 +7,7 @@ public class General {
         map.put("banana", 2);
         map.put("orange", 3);
         System.out.println("Value for key 'apple': " + map.get("apple"));
+        System.out.println("hashMap: " + map);
 
              HashSet<Integer> set =new HashSet<>();
         set.add(1);
@@ -14,6 +15,13 @@ public class General {
         set.add(3);
         set.add(2);//it will not print because set doesn't allow duplicate values
         System.out.println("Set: " + set);
+
+
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        treeMap.put("apple", 1);
+        treeMap.put("banana", 2);
+        treeMap.put("orange", 3);
+        System.out.println("TreeMap: " + treeMap);
 
    
     }
