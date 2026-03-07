e68f79f6635828730d7014db945e19b177503470
Piyush1817 <lakwanisuresh086@gmail.com>
2026-03-07 09:13:27 +0530

    Hash map

---
+++ hash/General.java
@@ -0,0 +1,13 @@
+package hash;
+import java.util.*;
+public class General {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("apple", 1);                
+        map.put("banana", 2);
+        map.put("orange", 3);
+        System.out.println("Value for key 'apple': " + map.get("apple"));
+
+   
+    }
+}
