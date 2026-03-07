15a5eca112f0c392724961911c1162d6c6563c8e
Piyush1817 <lakwanisuresh086@gmail.com>
2026-03-07 09:20:12 +0530

    Tree set

--- hash/General.java
+++ hash/General.java
@@ -10,9 +10,9 @@ public class General {
         System.out.println("hashMap: " + map);
 
              HashSet<Integer> set =new HashSet<>();
-        set.add(1);
-        set.add(2); 
-        set.add(3);
+        set.add(89);
+        set.add(3); 
+        set.add(2);
         set.add(2);//it will not print because set doesn't allow duplicate values
         System.out.println("Set: " + set);
 
@@ -23,6 +23,15 @@ public class General {
         treeMap.put("orange", 3);
         System.out.println("TreeMap: " + treeMap);
 
-   
+        // TreeMap will store the elements in sorted order
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(3);
+        treeSet.add(1);
+        treeSet.add(2);
+        System.out.println("TreeSet: " + treeSet);
+        // TreeSet will store the elements in sorted order  
+
+   //hashset is aslo store the elements in sorted order but it is not guaranteed that it will always store the elements in sorted order because it uses hashcode to store the elements and hashcode can be same for different elements so it may not store the elements in sorted order but treemap and treeset will always store the elements in sorted order because they use compareTo method to store the elements and compareTo method will always return a negative number if the first element is less than the second element and it will return a positive number if the first element is greater than the second element and it will return 0 if both the elements are equal so it will always store the elements in sorted order
     }
 }
