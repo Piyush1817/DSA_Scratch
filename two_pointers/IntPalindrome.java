e4346b818feeb8c62e6b2df0c702801e839ce714
Piyush1817 <lakwanisuresh086@gmail.com>
2026-04-08 12:38:04 +0530

    number is Palindrome or not Leetcode

---
+++ two_pointers/IntPalindrome.java
@@ -0,0 +1,36 @@
+package two_pointers;
+import java.util.*;
+
+public class IntPalindrome {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number:");
+        int x = sc.nextInt();
+
+        boolean result = isPalindrome(x);
+
+        System.out.println("Is Palindrome: " + result);
+    }
+
+    static boolean isPalindrome(int x){
+
+        String s = Integer.toString(x);
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start < end){
+
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
