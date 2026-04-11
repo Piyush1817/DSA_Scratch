c0bd24f5ad009335e578ef244cfa287c806848ea
Piyush1817 <lakwanisuresh086@gmail.com>
2026-04-11 10:15:31 +0530

    added another method to reverse string

--- recursion/ReverString.java
+++ recursion/ReverString.java
@@ -7,12 +7,23 @@ public class ReverString {
         String str = sc.nextLine();
         String result = reverseString(str);
         System.out.println("Reversed string: " + result);
+            System.out.println("Reversed string using method 2: ");
+     
+        System.out.println(reverseString2(str, str.length() - 1));
+           
     }
      public  static String reverseString(String str){
         if(str.length() == 0){
             return str; // base case
         }
         return reverseString(str.substring(1)) + str.charAt(0);
+    }
+    public static String reverseString2(String str , int index){
+        if (index<0){
+            return "";
+        }
+       
+        return str.charAt(index) + reverseString2(str,index-1);
     }
     
 }
