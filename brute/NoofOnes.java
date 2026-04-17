0f212d17387ccd3b36a59c6b35d223268f15780a
Piyush1817 <lakwanisuresh086@gmail.com>
2026-04-17 12:13:33 +0530

    without converting number to binary

--- brute/NoofOnes.java
+++ brute/NoofOnes.java
@@ -9,7 +9,9 @@ public class NoofOnes{
     int n= sc.nextInt();
     int count = numberOfOnes(n);
     System.out.println("Number of ones in binary of the number is :"+count);
-   }
+    int count2 = withoutConvertingBinary(n);
+    System.out.println("Number of ones without converting to binary :"+ count2);
+}
 
    public static int numberOfOnes(int n){
     String binary = Integer.toBinaryString(n);
@@ -18,6 +20,17 @@ public class NoofOnes{
         if(binary.charAt(i)=='1'){
             count++;
         }
+    }
+    return count ;
+   }
+
+   public static int withoutConvertingBinary(int n ){
+    int count = 0;
+    while(n>0){
+        if(n%2==1){
+            count++;
+        }
+        n=n/2;
     }
     return count ;
    }
