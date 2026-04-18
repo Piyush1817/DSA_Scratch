940c064dc56a16b203a9035d73cdc970c55b3010
Piyush1817 <lakwanisuresh086@gmail.com>
2026-04-18 22:00:19 +0530

    using optimal solution mirror distance

--- strings/MirrorDistance.java
+++ strings/MirrorDistance.java
@@ -8,6 +8,8 @@ public class MirrorDistance{
         int n = sc.nextInt();
         int result = mirrorDistance(n);
         System.out.println("Mirror distance of the number is :"+result);
+        int result = mirrorDistance2(n);    
+        System.out.println("Mirror distance of the number is :"+result);    
 
     }
     public static int mirrorDistance(int n){
@@ -16,5 +18,16 @@ public class MirrorDistance{
         sb.reverse();
             int reversed = Integer.parseInt(sb.toString());
        return Math.abs(n - reversed);
+    }
+    public static int mirrorDistance2(int n){
+        int temp = n;
+        int rev = 0;
+
+        while(temp > 0){
+            rev = rev * 10 + temp % 10;
+            temp = temp / 10;
+        }
+
+        return Math.abs(n - rev);
     }
 }
