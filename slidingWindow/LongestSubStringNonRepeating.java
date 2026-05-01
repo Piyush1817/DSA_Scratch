c7b5607e7b4b2d70c851b1a2ca6abbbf1f21530c
Piyush1817 <lakwanisuresh086@gmail.com>
2026-05-01 19:28:02 +0530

    longest substring without repeating characters using sliding window

---
+++ slidingWindow/LongestSubStringNonRepeating.java
@@ -0,0 +1,31 @@
+package slidingWindow;
+import java.util.HashSet;
+import java.util.Scanner;
+public class LongestSubStringNonRepeating {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+        int maxLength = longestSubString(str);
+        System.out.println("The length of the longest substring without repeating characters is: " + maxLength);
+    }
+    public static int longestSubString(String s){
+       
+   HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }}
