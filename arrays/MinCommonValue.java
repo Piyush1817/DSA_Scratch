902bf562530845c7f6d2972b067d843c8b66ca6e
Piyush1817 <lakwanisuresh086@gmail.com>
2026-05-19 10:06:04 +0530

    min common value leetcode

---
+++ arrays/MinCommonValue.java
@@ -0,0 +1,45 @@
+package arrays;
+
+import java.util.*;
+
+class MinCommonValue {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of first array: ");
+        int n1 = sc.nextInt();
+        int[] arr1 = new int[n1];
+        System.out.println("Enter the elements of first array: ");
+        for(int i=0; i<n1; i++){
+            arr1[i] = sc.nextInt();
+        }
+        System.out.println("Enter the size of second array: ");
+        int n2 = sc.nextInt();
+        int[] arr2 = new int[n2];
+        System.out.println("Enter the elements of second array: ");
+        for(int i=0; i<n2; i++){
+            arr2[i] = sc.nextInt();
+        }
+        int result = minCommonValue(arr1, arr2);
+        if(result != -1){
+            System.out.println("Minimum common value: " + result);
+        } else {
+            System.out.println("No common value found.");
+        }
+    }
+    public static int minCommonValue(int[] arr1, int[] arr2){
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int i = 0;
+        int j = 0;
+        while(i < arr1.length && j < arr2.length){
+            if(arr1[i] == arr2[j]){
+                return arr1[i];
+            } else if(arr1[i] < arr2[j]){
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
